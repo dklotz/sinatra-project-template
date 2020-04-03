689b3780a5305714be921195da65f8be7f7f57bd
David Klotz <david.klotz@zweitag.de>
2020-04-03 17:02:24 +0200

    Fix dotenv loading for local and or environment specific env files

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,14 @@ root_path = File.expand_path(File.join(File.dirname(__FILE__), '../app'))
 set :root, root_path
 set :app_file, "#{root_path}/main.rb"
 
-Dotenv.load
+# Sadly, the environment-specific override logic is in dotenv-rails, not dotenv
+# core, so we have to do this ourself (non-existing files are ignored):
+Dotenv.load(
+  File.expand_path("../.env.#{settings.environment}.local", __dir__),
+  File.expand_path("../.env.local", __dir__),
+  File.expand_path("../.env.#{settings.environment}", __dir__),
+  File.expand_path("../.env", __dir__)
+)
 
 require_all 'config/initializers'
 
